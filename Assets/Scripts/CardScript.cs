97e8101610ea802c08309f92c71c0d34db13f078
AxelRodriguezTapia <axel.rodriguez.tapia11@gmail.com>
2024-10-17 08:44:12 +0200

    jgd

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -7,7 +7,7 @@ public class CardScript : MonoBehaviour
 
     public GameObject figura;
     private GameObject gm;
-    private bool selected;
+    private int id;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +19,57 @@ public class CardScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            esconder();
+        }
+    }
 
+    public void setId(double idp){
+        id=(int)idp;
+        //set color
+    }
+
+    public int getId(){
+        return id;
     }
 
     void OnMouseDown()
     {
-        Debug.Log("Me apretaste guey");
-        gm.GetComponent<GameManager>().cardTouched(gameObject);
-
-        
+        //Trigger del game object
+        if(gm.GetComponent<GameManager>().canClickTrigger()){
+            gm.GetComponent<GameManager>().setClickTrigger(true);
+            AnimatorStateInfo stateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+            if(stateInfo.IsName("FigureDown")){
+                if(gm.gameObject.GetComponent<GameManager>().hiHaPuesto()){
+                    if(stateInfo.IsName("FigureDown")){
+                        Animator an = GetComponent<Animator>();
+                        an.SetTrigger("FigureShowTrigger");
+                    }
+                    gm.GetComponent<GameManager>().cardTouched(gameObject);
+                }else{
+                    Debug.Log("Ja hi ha cartes aixecades!");
+                }
+            }
+        }
     }
 
+    public void esconder(){
+        AnimatorStateInfo stateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName("FigureUp"))
+        {
+            Animator an = GetComponent<Animator>();
+            an.SetTrigger("FigureHideTrigger");
+        }
+    }
+
+    public void showFigure(){
+        AnimatorStateInfo stateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName("FigureDown"))
+        {
+            Animator an = GetComponent<Animator>();
+            an.SetTrigger("FigureShowTrigger");
+        }
+    }
+
+    
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] cards;
+    private double id_double=0;
+    private GameObject[] cardsSelected = new GameObject[2];
+    private int cartesAdivinades;
+    private bool clickTrigger=false;
+    private float clickCooldown=0;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        cardsSelected[0]=null;
+        cardsSelected[1]=null;
+        cartesAdivinades=0;
         cards = GameObject.FindGameObjectsWithTag("CardTag");
+        foreach(GameObject card in cards){
+            card.GetComponent<CardScript>().setId(id_double);
+            id_double=id_double+0.5;
+            Debug.Log(card.GetComponent<CardScript>().getId());
+        }
     
-
-        // Crear una matriz bidimensional (4x4) de GameObjects
-        GameObject[,] matriz2D = new GameObject[4, 4];
         int i=0;
 
         // Asignar e instanciar los objetos
@@ -23,7 +33,7 @@ public class GameManager : MonoBehaviour
             for (int y = 0; y < 4; y++)
             {
                 // Instanciar el prefab en una posición del espacio 3D
-                Vector3 posicion = new Vector3((x * 2)-3.5, 0, (y * 2)-4); // Ajustar la posición de cada objeto
+                Vector3 posicion = new Vector3((float)((x * 2)-3.5), 0, (y * 2)-4); // Ajustar la posición de cada objeto
                 cards[i].transform.position = posicion;
                 i++;
             }
@@ -34,14 +44,82 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(clickTrigger){
+            clickCooldown+=Time.deltaTime;
+            if(clickCooldown>=1){
+                clickCooldown=0;
+                clickTrigger=false;
+            }
+        }
+
+        if(cardsSelected[0]!=null && cardsSelected[1]!=null){
+            AnimatorStateInfo stateInfo0 = cardsSelected[0].gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+            AnimatorStateInfo stateInfo1 = cardsSelected[1].gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+            if(stateInfo0.IsName("FigureUp") && stateInfo1.IsName("FigureUp")){
+                checkIds();
+            }
+        }
+        if(cartesAdivinades==8){
+            Debug.Log("End Game");
+        }
         
+        
+    }
+    public bool canClickTrigger(){
+        return !clickTrigger;
+    }
+
+    public void setClickTrigger(bool clickTriger){
+        clickTrigger = clickTriger;
     }
 
     public void cardTouched(GameObject cardtouched){
-        Debug.Log("Me han tocado la carta "+cardtouched.name);
-
+        //Debug.Log("Me han tocado la carta "+cardtouched.name+" i te la id: "+cardtouched.GetComponent<CardScript>().getId());
+        
+        if(cardsSelected[0]==null && cardsSelected[1]==null){
+            cardsSelected[0]=cardtouched.gameObject;
+            //Debug.Log("Carta seleccionada");
+        }else{
+            cardsSelected[1]=cardtouched.gameObject;
+        }   
+        
+        
+        
     }
-    void bidimensionalTable(){
 
+    //public void allCardsDown(){
+    //    foreach(GameObject card in cards){
+    //        card.GetComponent<CardScript>().moveDown();
+    //    }
+    //}
+    public void checkIds(){
+        if(cardsSelected[0]!=null && cardsSelected[1]!=null){
+            if(cardsSelected[0].GetComponent<CardScript>().getId()!=cardsSelected[1].GetComponent<CardScript>().getId()){
+                cardsSelected[0].GetComponent<CardScript>().esconder();
+                cardsSelected[1].GetComponent<CardScript>().esconder();
+                borrarSeleccionados(12);
+            }else{
+                //Debug.Log("Cartas iguals!!" +cardsSelected[0].GetComponent<CardScript>().getId()+" "+cardsSelected[1].GetComponent<CardScript>().getId());
+                cartesAdivinades++;
+                borrarSeleccionados(12);
+            }
+        }
+    }
+
+    public bool hiHaPuesto(){
+        return cardsSelected[0]==null || cardsSelected[1]==null;
+    }
+
+    public void borrarSeleccionados(int num){
+        if(num==0){
+            cardsSelected[0]=null;
+        }
+        if(num==1){
+            cardsSelected[1]=null;
+        }
+        if(num==12){
+            cardsSelected[0]=null;
+            cardsSelected[1]=null;
+        }
     }
 }
